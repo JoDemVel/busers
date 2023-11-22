b12fa3990c03346705ad8ab16c4d519e63a0e23c
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2023-11-21 21:00:27 -0400

    Add base project

---
+++ src/test/java/com/jdve/busers/BackendUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdve.busers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
